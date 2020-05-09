092ee6c866603646b37c268131f36994ee11e695
parkjunghun <go9018@naver.com>
2020-05-09 19:06:32 +0900

    init camera sample App

--- CameraSample/flutter_camera_sample/test/widget_test.dart
+++ CameraSample/flutter_camera_sample/test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fluttercamerasample/main.dart';
+import 'package:fluttercamerasample/pages/AccountPage.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(AccountPage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
